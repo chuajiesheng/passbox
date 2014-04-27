343f26f35d7b46e70b491eba6d9aa4d7b587d18f
Chua Jie Sheng <chuajiesheng@gmail.com>
2014-04-27 22:34:59 +0800

    Allow multi-level templating using {base, head, content}

--- test/template/http.go
+++ test/template/http.go
@@ -1,22 +1,88 @@
 package template
 
 import (
+	"bytes"
 	"html/template"
 	"net/http"
+	"strings"
 )
 
 func init() {
-	http.HandleFunc("/test/template", handler)
+	http.HandleFunc("/test/template/", handler)
 }
 
-type Var struct {
-        User     string
-        Script   string
-	Content  template.HTML
+type baseContent struct {
+	HeadHTML    template.HTML
+	NavbarHTML  template.HTML
+	ContentHTML template.HTML
+}
+
+type headContent struct {
+	Script template.HTMLAttr
+}
+
+type navbarContent struct {
+	User string
+}
+
+func handleURL(requestURL string, url string) bool {
+	return strings.HasSuffix(requestURL, url)
+}
+
+func parseTemplate(file string, data interface{}) (out []byte, error error) {
+	var buf bytes.Buffer
+	t, err := template.ParseFiles(file)
+	if err != nil {
+		return nil, err
+	}
+	err = t.Execute(&buf, data)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func getHello() (out []byte) {
+	head, error :=
+		parseTemplate(
+		"template/head.html",
+		headContent{Script: template.HTMLAttr("hello.js")},
+	)
+	if error != nil {
+		return []byte("Internal server error...")
+	}
+
+	navbar, error :=
+		parseTemplate(
+		"template/navbar.html",
+		navbarContent{User: "Sergey Pimenov"},
+	)
+	if error != nil {
+		return []byte("Internal server error...")
+	}
+
+	content := template.HTML("<div class=\"row\"><div class=\"span12\"><h1>Hello World</h1></div></div>")
+
+	final, error := parseTemplate("template/base.html", baseContent{
+		HeadHTML:    template.HTML(head),
+		NavbarHTML:  template.HTML(navbar),
+		ContentHTML: template.HTML(content)})
+	if error != nil {
+		return []byte("Internal server error...")
+	}
+
+	return final
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	t := template.Must(template.ParseGlob("template/*"))
-	p := Var{User: "Jimmy", Script: "hello.js", Content: template.HTML("<div class=\"row\"><div class=\"span12\"><h1>Hello World</h1></div></div>")}
-	t.ExecuteTemplate(w, "base", p)
+	requestURL := r.URL.String()
+
+	if handleURL(requestURL, "template/hello") {
+		page := getHello()
+		w.Write(page)
+	}
+
+	if handleURL(requestURL, "template/add") {
+
+	}
 }
