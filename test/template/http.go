f28c4a1db39f3fb7643fd7b4daa6f7b7670bc881
Chua Jie Sheng <chuajiesheng@gmail.com>
2014-05-01 14:54:03 +0800

    Tested template for answer.html

--- test/template/http.go
+++ test/template/http.go
@@ -25,6 +25,11 @@ type navbarContent struct {
 	User string
 }
 
+type answerContent struct {
+	Username string
+	Password string
+}
+
 func handleURL(requestURL string, url string) bool {
 	return strings.HasSuffix(requestURL, url)
 }
@@ -42,26 +47,16 @@ func parseTemplate(file string, data interface{}) (out []byte, error error) {
 	return buf.Bytes(), nil
 }
 
-func getHello() (out []byte) {
-	head, error :=
-		parseTemplate(
-		"template/head.html",
-		headContent{Script: template.HTMLAttr("hello.js")},
-	)
+func generatePage(h headContent, n navbarContent, content []byte) (out []byte) {
+	head, error := parseTemplate("template/head.html", h)
 	if error != nil {
 		return []byte("Internal server error...")
 	}
 
-	navbar, error :=
-		parseTemplate(
-		"template/navbar.html",
-		navbarContent{User: "Sergey Pimenov"},
-	)
+	navbar, error := parseTemplate("template/navbar.html", n)
 	if error != nil {
 		return []byte("Internal server error...")
 	}
-
-	content := template.HTML("<div class=\"row\"><div class=\"span12\"><h1>Hello World</h1></div></div>")
 
 	final, error := parseTemplate("template/base.html", baseContent{
 		HeadHTML:    template.HTML(head),
@@ -72,56 +67,68 @@ func getHello() (out []byte) {
 	}
 
 	return final
+
 }
 
-func getAdd() (out []byte) {
-	head, error :=
-		parseTemplate(
-		"template/head.html",
-		headContent{Script: template.HTMLAttr("add.js")},
-	)
-	if error != nil {
+func getHello(h headContent, n navbarContent) (out []byte) {
+	content := []byte("<div class=\"row\"><div class=\"span12\"><h1>Hello World</h1></div></div>")
+	return generatePage(h, n, content)
+}
+
+func getAdd(h headContent, n navbarContent) (out []byte) {
+	content, error := parseTemplate("template/add.html", nil)
+	if error == nil {
+		return generatePage(h, n, content)
+	} else {
 		return []byte("Internal server error...")
 	}
+}
 
-	navbar, error :=
-		parseTemplate(
-		"template/navbar.html",
-		navbarContent{User: "Sergey Pimenov"},
-	)
-	if error != nil {
+func getAnswer(h headContent, n navbarContent, a answerContent) (out []byte) {
+	content, error := parseTemplate("template/answer.html",	a)
+	if error == nil {
+		return generatePage(h, n, content)
+	} else {
 		return []byte("Internal server error...")
 	}
-
-	content, error :=
-		parseTemplate(
-		"template/add.html",
-		nil)
-	if error != nil {
-		return []byte("Internal server error...")
-	}
-
-	final, error := parseTemplate("template/base.html", baseContent{
-		HeadHTML:    template.HTML(head),
-		NavbarHTML:  template.HTML(navbar),
-		ContentHTML: template.HTML(content)})
-	if error != nil {
-		return []byte("Internal server error...")
-	}
-
-	return final
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	requestURL := r.URL.String()
+	var n = navbarContent{User: "Sergey Pimenov"}
 
 	if handleURL(requestURL, "template/hello") {
-		page := getHello()
+		var h = headContent{Script: template.HTMLAttr("hello.js")}
+		page := getHello(h, n)
 		w.Write(page)
 	}
 
 	if handleURL(requestURL, "template/add") {
-		page := getAdd()
+		var h = headContent{Script: template.HTMLAttr("add.js")}
+		page := getAdd(h, n)
 		w.Write(page)
+	}
+
+	if handleURL(requestURL, "template/answer") {
+		var h = headContent{Script: template.HTMLAttr("empty.js")}
+		var a = answerContent{Username: "HelloWorld@gmail.com", Password: "p@s5W0rd!"}
+		page := getAnswer(h, n, a)
+		w.Write(page)
+	}
+
+	if handleURL(requestURL, "template/home") {
+
+	}
+
+	if handleURL(requestURL, "template/query") {
+
+	}
+
+	if handleURL(requestURL, "template/setup") {
+
+	}
+
+	if handleURL(requestURL, "template/setup2") {
+
 	}
 }
