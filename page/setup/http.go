30175aac2aa4ad3fe9b9dc81306de98d6b2df96c
Chua Jie Sheng <chuajiesheng@gmail.com>
2014-05-05 22:30:08 +0800

    Refactor function into method

--- page/setup/http.go
+++ page/setup/http.go
@@ -13,18 +13,37 @@ func init() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	// check if exist security question
+
 	var n = helper.GetNavbarContent(w, r)
 	var h = e.HeadContent{Script: template.HTMLAttr("setup.js")}
 	page := helper.GetPlainPage(h, n, "template/setup.html")
 	w.Write(page)
 }
 
+func showSetup2Page(w http.ResponseWriter, r *http.Request) {
+	var h = e.HeadContent{Script: template.HTMLAttr("setup2.js")}
+	var n = helper.GetNavbarContent(w, r)
+	page := helper.GetPlainPage(h, n, "template/setup2.html")
+	w.Write(page)
+}
+
+func showErrorPage(w http.ResponseWriter, r *http.Request) {
+	var h = e.HeadContent{Script: template.HTMLAttr("empty.js")}
+	var n = helper.GetNavbarContent(w, r)
+	var m = e.MessageContent{
+		Header: "Sorry! Something just went wrong.",
+		Message: "Please let me know your question answer again.!",
+		Link: "/setup",
+		LinkTitle:"Security Question"}
+	page := helper.GetMessage(h, n, m)
+	w.Write(page)
+}
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
 	c, u := helper.RetrieveUser(w, r)
 
 	qas := []e.QuestionAnswer{}
-
 	qas = helper.AppendIfValid(qas, r.FormValue("qns1"), r.FormValue("ans1"))
 	qas = helper.AppendIfValid(qas, r.FormValue("qns2"), r.FormValue("ans2"))
 	qas = helper.AppendIfValid(qas, r.FormValue("qns3"), r.FormValue("ans3"))
@@ -35,20 +54,9 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 
 	if error == nil {
 		c.Infof("[page/setup/http.go] Inserted: %s", key)
-		var h = e.HeadContent{Script: template.HTMLAttr("setup2.js")}
-		var n = helper.GetNavbarContent(w, r)
-		page := helper.GetPlainPage(h, n, "template/setup2.html")
-		w.Write(page)
+		showSetup2Page(w, r)
 	} else {
-		var h = e.HeadContent{Script: template.HTMLAttr("empty.js")}
-		var n = helper.GetNavbarContent(w, r)
-		var m = e.MessageContent{
-			Header: "Sorry! Something just went wrong.",
-			Message: "Please let me know your question answer again.!",
-			Link: "/setup",
-			LinkTitle:"Security Question"}
-		page := helper.GetMessage(h, n, m)
-		w.Write(page)
+		showErrorPage(w, r)
 	}
 
 	sq2, err := helper.GetSecurityQuestion(r, (*u).Email)
